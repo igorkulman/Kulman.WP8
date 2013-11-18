8f76830a71635be329303b24d050016db724c624
Igor Kulman <igor@kulman.sk>
2013-11-18 19:58:01 +0100

    + SortedObservableCollection

---
+++ Kulman.WP8/Code/SortedObservableCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Kulman.WP8.Code
+{
+    public class SortedObservableCollection<T> : ObservableCollection<T>
+    {
+        private readonly Func<T, int> _func;
+
+        public SortedObservableCollection(Func<T, int> func)
+        {
+            _func = func;
+        }
+
+        public SortedObservableCollection(Func<T, int> func, IEnumerable<T> collection)
+            : base(collection)
+        {
+            _func = func;
+        }
+
+        public SortedObservableCollection(Func<T, int> func, List<T> list)
+            : base(list)
+        {
+            _func = func;
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+
+        protected override void InsertItem(int index, T item)
+        {
+            bool added = false;
+            for (int idx = 0; idx < Count; idx++)
+            {
+                if (_func(item) < _func(Items[idx]))
+                {
+                    base.InsertItem(idx, item);
+                    added = true;
+                    break;
+                }
+            }
+
+            if (!added)
+            {
+                base.InsertItem(index, item);
+            }
+        }
+    }
+}
