ff53fc9c1017eeedfb7348e46a03dda5be986810
Igor Kulman <igor@kulman.sk>
2013-11-18 19:48:29 +0100

    + init + settings interface

---
+++ Kulman.WP8/Interfaces/ISettingsService.cs
@@ -0,0 +1,23 @@
+﻿namespace Kulman.WP8.Interfaces
+{
+    /// <summary>
+    /// Interface definition for settings manipulation
+    /// </summary>
+    public interface ISettingsService
+    {
+        /// <summary>
+        /// Gets a storred value for a given key
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <returns>Value</returns>
+        object Get(string key);
+
+        /// <summary>
+        /// Save a key-value pair to settings
+        /// Overwites existing
+        /// </summary>
+        /// <param name="key">Key</param>
+        /// <param name="value">Value</param>
+        void Set(string key, object value);
+    }
+}
