50a75dd3bf7f453b49e42e9f056a7bd9e4ab13cc
Igor Kulman <igor@kulman.sk>
2013-11-26 15:28:24 +0100

    + address book service

---
+++ Kulman.WP8/Interfaces/IAddressBookService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Phone.UserData;
+
+namespace Kulman.WP8.Interfaces
+{
+    public interface IAddressBookService
+    {
+        /// <summary>
+        /// Gets a list of contacts in device address book
+        /// </summary>
+        /// <returns>List of contact</returns>
+        Task<List<Contact>> GetContacts();
+
+        /// <summary>
+        /// Gets a list of contacts in device address book filtered by search criteria
+        /// </summary>
+        /// <param name="searchTerm">Search term</param>
+        /// <param name="filter">Filter</param>
+        /// <returns>List of contact</returns>
+        Task<List<Contact>> Search(string searchTerm, FilterKind filter);
+    }
+}
---
+++ Kulman.WP8/Services/AddressBookService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kulman.WP8.Interfaces;
+using Microsoft.Phone.UserData;
+
+namespace Kulman.WP8.Services
+{
+    /// <summary>
+    /// Address book wrapper
+    /// Needs address book permissions to work
+    /// </summary>
+    public class AddressBookService : IAddressBookService
+    {
+        /// <summary>
+        /// Gets a list of contacts in device address book
+        /// </summary>
+        /// <returns>List of contact</returns>
+        public Task<List<Contact>> GetContacts()
+        {
+            var t = new TaskCompletionSource<List<Contact>>();
+            SearchContacts(s => t.TrySetResult(s));
+            return t.Task;
+        }
+
+        /// <summary>
+        /// Gets a list of contacts in device address book filtered by search criteria
+        /// </summary>
+        /// <param name="searchTerm">Search term</param>
+        /// <param name="filter">Filter</param>
+        /// <returns>List of contact</returns>
+        public Task<List<Contact>> Search(string searchTerm, FilterKind filter)
+        {
+            var t = new TaskCompletionSource<List<Contact>>();
+            SearchContacts(s => t.TrySetResult(s),filter,searchTerm);
+            return t.Task;
+        }
+
+        private void SearchContacts(Action<List<Contact>> callback, FilterKind filter = FilterKind.None, string searchTerm = null)
+        {
+            var cons = new Contacts();
+            cons.SearchCompleted += (s, e) => callback(e.Results.ToList());
+            cons.SearchAsync(String.IsNullOrEmpty(searchTerm) ? string.Empty : searchTerm, filter, null);
+        }
+    }
+}
