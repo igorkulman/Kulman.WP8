5f4d14110ff6cfaa4e924c8c7fb4243b5783471e
Igor Kulman <igor@kulman.sk>
2014-02-06 12:49:19 +0100

    + added attributes for use with automatic registration into a DI container

---
+++ Kulman.WP8/Attributes/PerRequestAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Kulman.WP8.Attributes
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public class PerRequestAttribute : Attribute
+	{
+	}
+}
