fefb441b20c3d6a858701e56f3fe2d787449d192
Igor Kulman <igor@kulman.sk>
2014-04-28 21:34:18 +0200

    * service for checking for app updates

---
+++ Kulman.WP8/Interfaces/IWindowsPhoneStoreUpdateService.cs
@@ -0,0 +1,13 @@
+﻿namespace Kulman.WP8.Interfaces
+{
+    public interface IWindowsPhoneStoreUpdateService
+    {
+        /// <summary>
+        /// Checks the Windows Phone Store to see if a newer version of the app is available
+        /// If it is, a dialog is shown
+        /// </summary>
+        /// <param name="updateDialogText">Dialog text</param>
+        /// <param name="updateDialogTitle">Dialog title</param>
+        void CheckForUpdatedVersion(string updateDialogText, string updateDialogTitle);
+    }
+}
