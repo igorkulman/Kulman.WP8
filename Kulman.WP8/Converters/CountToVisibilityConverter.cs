94ece71dce644b92a881a10307342fc8805ed5fe
Igor Kulman <igor@kulman.sk>
2014-04-29 15:23:32 +0200

    + CountToVisibilityConverter

---
+++ Kulman.WP8/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulman.WP8.Converters.Abstract;
+
+namespace Kulman.WP8.Converters
+{
+    public class CountToVisibilityConverter: BaseVisibilityConverter<int>
+    {
+        public bool IsInverted { get; set; }
+
+        protected override bool? ConvertToVisibility(int value)
+        {
+            return (IsInverted) ? value==0 : value > 0;
+        }
+    }
+}
