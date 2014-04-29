f50cc97dc4ec797ae5ce5b5a74c4184d57c78e2c
Igor Kulman <igor@kulman.sk>
2014-04-29 16:26:26 +0200

    + tests added

---
+++ Kulman.WP8.Tests/ConvertersTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using Kulman.WP8.Converters;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Kulman.WP8.Tests
+{
+    [TestClass]
+    public class ConvertersTests
+    {
+        [TestMethod]
+        public void BooleanToNegateConverterShouldWork()
+        {
+            var converter = new BooleanNegateConverter();
+            Assert.IsTrue((bool)converter.Convert(false,typeof(bool),null,CultureInfo.CurrentCulture));
+
+            Assert.IsFalse((bool)converter.Convert(true, typeof(bool), null, CultureInfo.CurrentCulture));
+        }
+
+        [TestMethod]
+        public void BooleanToVisibilityConverterShouldWork()
+        {
+            var converter = new BooleanToVisibilityConverter();
+            Assert.AreEqual(Visibility.Visible, (Visibility)converter.Convert(true, typeof(bool), null, CultureInfo.CurrentCulture));
+            Assert.AreEqual(Visibility.Collapsed, (Visibility)converter.Convert(false, typeof(bool), null, CultureInfo.CurrentCulture));
+        }
+
+        [TestMethod]
+        public void BooleanToVisibilityConverterShouldWorkInverterd()
+        {
+            var converter = new BooleanToVisibilityConverter()
+                            {
+                                IsInverted = true
+                            };
+            Assert.AreEqual(Visibility.Collapsed, (Visibility)converter.Convert(true, typeof(bool), null, CultureInfo.CurrentCulture));
+            Assert.AreEqual(Visibility.Visible, (Visibility)converter.Convert(false, typeof(bool), null, CultureInfo.CurrentCulture));
+        }
+    }
+}
---
+++ Kulman.WP8.Tests/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Kulman.WP8.Tests.Resources;
+
+namespace Kulman.WP8.Tests
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
