6d915653e9f27c22e8bf4cad74c4abb272d431aa
Igor Kulman <igor@kulman.sk>
2014-04-29 15:48:09 +0200

    * NullToVisibilityConverter changed to a more general ObjectToVisibilityConverter

---
+++ Kulman.WP8/Converters/ObjectToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulman.WP8.Converters.Abstract;
+
+namespace Kulman.WP8.Converters
+{
+    public class ObjectToVisibilityConverter : BaseVisibilityConverter<object>
+    {
+        protected override bool? ConvertToVisibility(object value)
+        {
+            if (value is string)
+            {
+                return !String.IsNullOrWhiteSpace((string)value);
+            }
+
+            if (value is IEnumerable)
+            {
+                return ((IEnumerable)value).Cast<object>().Any();
+            }
+
+            return value != null;
+        }
+    }
+}
