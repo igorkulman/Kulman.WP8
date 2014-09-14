7c0aa94943e78dd1e4e96ebe9f4d30dcda630768
Igor Kulman <igor@kulman.sk>
2014-09-14 19:32:10 +0200

    + added ManifestHelper with tests

---
+++ Kulman.WP8.Tests/ManifestHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kulman.WP8.Code;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Kulman.WP8.Tests
+{
+    [TestClass]
+    public class ManifestHelperTests
+    {
+        [TestMethod]
+        public void VersionShouldBeFetched()
+        {
+            var version = ManifestHelper.Version;
+            Assert.IsFalse(String.IsNullOrEmpty(version));
+        }
+
+        [TestMethod]
+        public void AuthorShouldBeFetched()
+        {
+            var author = ManifestHelper.Author;
+            Assert.IsFalse(String.IsNullOrEmpty(author));
+        }
+
+        [TestMethod]
+        public void TitleShouldBeFetched()
+        {
+            var title = ManifestHelper.Title;
+            Assert.IsFalse(String.IsNullOrEmpty(title));
+        }
+
+        [TestMethod]
+        public void GuidShouldBeFetched()
+        {
+            var guid = ManifestHelper.Guid;
+            Assert.IsFalse(guid==Guid.Empty);
+        }
+    }
+}
---
+++ Kulman.WP8/Code/ManifestHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Xml;
+
+namespace Kulman.WP8.Code
+{
+    /// <summary>
+    /// Helper class to get info from app manifest
+    /// </summary>
+    public static class ManifestHelper
+    {
+        /// <summary>
+        /// Gest an attribute from the manifest by name
+        /// </summary>
+        /// <param name="attributeName">Attribute name</param>
+        /// <returns>Attribute value</returns>                
+        private static string GetAppAttribute(string attributeName)
+        {
+            const string appManifestName = "WMAppManifest.xml";
+            const string appNodeName = "App";
+
+            var settings = new XmlReaderSettings { XmlResolver = new XmlXapResolver() };
+
+            using (var rdr = XmlReader.Create(appManifestName, settings))
+            {
+                rdr.ReadToDescendant(appNodeName);
+                if (!rdr.IsStartElement())
+                {
+                    throw new FormatException(appManifestName + " is missing " + appNodeName);
+                }
+
+                return rdr.GetAttribute(attributeName);
+            }
+        }
+
+        /// <summary>
+        /// Application title
+        /// </summary>        
+        public static string Title
+        {
+            get
+            {
+                return GetAppAttribute("Title");
+            }
+        }
+
+        /// <summary>
+        /// Application version
+        /// </summary>        
+        public static string Version
+        {
+            get
+            {
+                return GetAppAttribute("Version");
+            }
+        }
+
+        /// <summary>
+        /// Application author
+        /// </summary>        
+        public static string Author
+        {
+            get
+            {
+                return GetAppAttribute("Author");
+            }
+        }
+
+        /// <summary>
+        /// Application author
+        /// </summary>        
+        public static Guid Guid
+        {
+            get
+            {
+                return Guid.Parse(GetAppAttribute("ProductID"));
+            }
+        }
+    }
+}
