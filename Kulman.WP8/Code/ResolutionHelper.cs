f20c10bc753ccdb6d5a07d4b2524e6ac34bbd1f6
Igor Kulman <igor@kulman.sk>
2015-04-29 13:36:57 +0200

    + new helper classes

---
+++ Kulman.WP8/Code/ResolutionHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using Microsoft.Phone.Info;
+
+namespace Kulman.WP8.Code
+{
+    public static class ResolutionHelper
+    {
+        private static int? _scaleFactor;
+
+        static ResolutionHelper()
+        {
+            object physicalScreenResolutionObject;
+
+            if (DeviceExtendedProperties.TryGetValue("PhysicalScreenResolution", out physicalScreenResolutionObject))
+            {
+                var physicalScreenResolution = (Size)physicalScreenResolutionObject;
+
+                _scaleFactor= (int)(physicalScreenResolution.Width / 4.8);
+            }
+
+            _scaleFactor= Application.Current.Host.Content.ScaleFactor;
+        }
+
+        private static bool IsWvga
+        {
+            get { return _scaleFactor.HasValue && _scaleFactor.Value == 100; }
+        }
+
+        private static bool IsWxga
+        {
+            get { return _scaleFactor.HasValue && _scaleFactor.Value == 160; }
+        }
+
+        private static bool Is720p
+        {
+            get { return _scaleFactor.HasValue && _scaleFactor.Value == 150; }
+        }
+
+        private static bool Is1080p
+        {
+            get { return _scaleFactor.HasValue && _scaleFactor.Value == 225; }
+        }
+
+        public static Size CurrentResolution
+        {
+            get
+            {
+                if (IsWxga) return new Size(1280,768);
+                if (Is720p) return new Size(1280,720);
+                if (Is1080p) return new Size(1920,1080);
+                return new Size(480,800);
+            }
+        }        
+    }
+}
---
+++ Kulman.WP8/Code/ResourceHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using JetBrains.Annotations;
+
+namespace Kulman.WP8.Code
+{
+    /// <summary>
+    /// Helper class for accessing resources from code
+    /// </summary>
+    public static class ResourceHelper
+    {       
+        /// <summary>
+        /// Gets a resource by name
+        /// </summary>
+        /// <typeparam name="T">Resource type</typeparam>
+        /// <param name="name">Resource name</param>
+        /// <returns>Resource</returns>
+        [NotNull]
+        public static T Get<T>([NotNull]string name)
+        {
+            return (T)Application.Current.Resources[name];
+        }
+    }
+}
